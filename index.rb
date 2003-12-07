c36a30453823cb2eddcb35db9612954a5883f04f
Minero Aoki <mineroaoki@gmail.com>
2003-12-07 00:53:01 +0000

    * index.rb: basedir -> cgidir.
    git-svn-id: file:///Users/aamine/c/bitchannel/ttmp/public/bitchannel/trunk@687 1b9489fe-b721-0410-924e-b54b9192deb8

--- index.rb
+++ index.rb
@@ -5,20 +5,20 @@
 
 $KCODE = 'EUC'
 
-basedir = File.dirname(File.expand_path(__FILE__))
+cgidir = File.dirname(File.expand_path(__FILE__))
 
-$:.unshift "#{basedir}/lib"
+$:.unshift "#{cgidir}/lib"
 require 'wikitik'
 require 'wikitik/rc/ja'
 
 repo = Wikitik::Repository.new(
   :cmd_path  => '/usr/bin/cvs',
   # :cvsroot => '/home/aamine/var/cvs/test',
-  :wc_read   => "#{basedir}/wc.read",
-  :wc_write  => "#{basedir}/wc.write"
+  :wc_read   => "#{cgidir}/wc.read",
+  :wc_write  => "#{cgidir}/wc.write"
 )
 config = Wikitik::Config.new(
-  :templatedir => "#{basedir}/template",
+  :templatedir => "#{cgidir}/template",
   :charset => 'euc-jp',
   :css_url => 'default.css',
   :cgi_url => 'index.rb'
