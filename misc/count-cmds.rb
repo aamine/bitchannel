2cd292a93efcfeb328ff553f2b00ffb54adb4782
Minero Aoki <mineroaoki@gmail.com>
2005-03-16 20:20:39 +0000

    * misc/count-cmds.rb: do not specify relative URL.
    git-svn-id: file:///Users/aamine/c/bitchannel/ttmp/public/bitchannel/trunk@1003 1b9489fe-b721-0410-924e-b54b9192deb8

--- misc/count-cmds.rb
+++ misc/count-cmds.rb
@@ -5,11 +5,9 @@
 # count bitchannel commands from Apache's access.log
 #
 
-CGI_URL_RE = %r</w/index.cgi>
-
 table = Hash.new(0)
 ARGF.each do |line|
-  cmd = line.slice(%r<#{CGI_URL_RE}?cmd=(\w+)>o, 1)
+  cmd = line.slice(%r<\?cmd=(\w+)>, 1)
   table[cmd] += 1 if cmd
 end
 table.to_a.sort_by {|cmd, num| -num }.each do |cmd, num|
